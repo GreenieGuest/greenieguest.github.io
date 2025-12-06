cb01a9acda6506b7d4b1e15b9694102de528ab8d
GreenieGuest <papootreasure@yahoo.com>
2025-12-06 02:43:57 -0600

    fix

--- js/ewowicosathlon.js
+++ js/ewowicosathlon.js
@@ -53,7 +53,7 @@ function load() {
 			}
 		});
 
-		const samplecast = sample(names, castSize);
+		const samplecast = sample(names, castSize - 1);
 		
 		athletes = samplecast.map((item, index) => new Player(item, 0, index, roundScores[names.indexOf(item)]));
 		stillInTheRunning(athletes, false);
