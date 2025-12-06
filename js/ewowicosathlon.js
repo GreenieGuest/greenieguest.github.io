d881f56c3459a55541b6f9d695d168c62ef1f964
GreenieGuest <papootreasure@yahoo.com>
2025-12-06 02:49:54 -0600

    fix

--- js/ewowicosathlon.js
+++ js/ewowicosathlon.js
@@ -8,7 +8,7 @@ const base_points = [100, 70, 50, 40, 30, 25, 20, 15, 12, 10, 8, 6, 4, 3, 2, 1];
 const names = [];
 const colors = [];
 
-const roundScores = Array.from({ length: 16 }, () => []);
+const roundScores = Array.from({ length: 220 }, () => []);
 
 let athletes = [];
 let bootOrder = [];
